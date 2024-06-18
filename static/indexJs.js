cabfb810351780f5e427c75a0658e1e0e6d237f6
pablonara <pablonara@pablonara.com>
2024-06-17 22:02:10 -0700

    Auto-commit

--- static/indexJs.js
+++ static/indexJs.js
@@ -56,14 +56,3 @@ ScrollTrigger.create({
   markers: true
 });
 
-document.querySelector("button").addEventListener("click", e => {
-  // scroll to the spot where .box-c is in the center.
-  // parameters: element, smooth, position
-  smoother.scrollTo(".box-c", true, "center center");
-  
-  // or you could animate the scrollTop:
-  // gsap.to(smoother, {
-  //  scrollTop: smoother.offset(".box-c", "center center"),
-  //  duration: 1
-  // });
-});
