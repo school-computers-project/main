a2c9762a0c792e68c6bb10aedc402353aa2a03c9
pablonara <pablonara@pablonara.com>
2024-06-18 21:40:31 -0700

    i have no idea why this doesn't work and i spent 5 hours on this

--- static/indexJs.js
+++ static/indexJs.js
@@ -43,18 +43,38 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollTrigger.normalizeScroll(true)
 
 // create the smooth scroller FIRST!
-let smoother = ScrollSmoother.create({
-  smooth: 2,
-  effects: true,
-  normalizeScroll: true
-});
+// let smoother = ScrollSmoother.create({
+//   smooth: 2,
+//   effects: true,
+//   normalizeScroll: true
+// });
 
-// pin box-c when it reaches the center of the viewport, for 300px
+// // pin box-c when it reaches the center of the viewport, for 300px
 // ScrollTrigger.create({
-//   trigger: ".box-c",
+//   trigger: ".secondContainer",
 //   pin: true,
 //   start: "center center",
 //   end: "+=300",
 //   markers: true
 // });
+console.log(document.querySelector("#smooth-content")); // ?
 
+ScrollSmoother.create({
+    smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+    effects: true, // looks for data-speed and data-lag attributes on elements
+    smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+  });
+
+  let smoother = ScrollSmoother.create({
+    smooth: 2,
+    effects: true,
+    normalizeScroll: true
+  });
+  
+  ScrollTrigger.create({
+    trigger: ".secondContainer",
+    pin: true,
+    start: "100vh",
+    end: "+=0",
+    markers: true //remove when fix
+  });
