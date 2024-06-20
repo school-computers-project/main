b54d471e25b79465d49cf0124c5e97595b6b9d89
pablonara <pablonara@pablonara.com>
2024-06-19 21:22:17 -0700

    Auto-commit

--- static/indexJs.js
+++ static/indexJs.js
@@ -106,16 +106,16 @@ ScrollTrigger.create({
   }
 });
 
-function isInViewport(element) {
-  var rect = element.getBoundingClientRect();
-  var html = document.documentElement;
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || html.clientHeight) &&
-    rect.right <= (window.innerWidth || html.clientWidth)
-  );
-}
+// function isInViewport(element) {
+//   var rect = element.getBoundingClientRect();
+//   var html = document.documentElement;
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= (window.innerHeight || html.clientHeight) &&
+//     rect.right <= (window.innerWidth || html.clientWidth)
+//   );
+// }
 
 async function checkIsInViewport() {
   setInterval(async () => {
