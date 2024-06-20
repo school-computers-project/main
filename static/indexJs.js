74c5d7fcad2469534cff157579e7be01ce0b15bc
pablonara <pablonara@pablonara.com>
2024-06-19 21:02:25 -0700

    Auto-commit

--- static/indexJs.js
+++ static/indexJs.js
@@ -93,16 +93,16 @@ panels.forEach((panel, i) => {
   });
 });
 
-// ScrollTrigger.create({
-//   snap: {
-//     snapTo: (progress, self) => {
-//       let panelStarts = tops.map(st => st.start), // an Array of all the starting scroll positions. We do this on each scroll to make sure it's totally responsive. Starting positions may change when the user resizes the viewport
-//         snapScroll = gsap.utils.snap(panelStarts, self.scroll()); // find the closest one
-//       return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll); // snapping requires a progress value, so convert the scroll position into a normalized progress value between 0 and 1
-//     },
-//     duration: 0.5
-//   }
-// });
+ScrollTrigger.create({
+  snap: {
+    snapTo: (progress, self) => {
+      let panelStarts = tops.map(st => st.start), // an Array of all the starting scroll positions. We do this on each scroll to make sure it's totally responsive. Starting positions may change when the user resizes the viewport
+        snapScroll = gsap.utils.snap(panelStarts, self.scroll()); // find the closest one
+      return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll); // snapping requires a progress value, so convert the scroll position into a normalized progress value between 0 and 1
+    },
+    duration: 0.5
+  }
+});
 
 
 var tl,
