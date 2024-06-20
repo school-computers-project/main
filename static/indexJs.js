3d9b8ee9ca83d39cd456f373982eb57c8d730168
pablonara <pablonara@pablonara.com>
2024-06-19 21:25:33 -0700

    Auto-commit

--- static/indexJs.js
+++ static/indexJs.js
@@ -66,7 +66,7 @@ ScrollSmoother.create({
 });
 
 let smoother = ScrollSmoother.create({
-  smooth: 2,
+  smooth: 1,
   effects: true,
   normalizeScroll: true,
   smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
@@ -106,22 +106,16 @@ ScrollTrigger.create({
   }
 });
 
-
-async function checkIsInViewport() {
-  setInterval(async () => {
-    // Assuming 'element' is defined somewhere accessible within this scope
-    if (isInViewport(".containerBackground")) {
-      console.log('Element is in viewport');
-      // Trigger your desired action here
-    } else {
-      console.log('Element is not in viewport');
-      // Optionally handle the case where the element is not in viewport
-    }
-  }, 1000); // Check every 1000 milliseconds (1 second)
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  var html = document.documentElement;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
 }
-
-// Example usage:
-checkIsInViewport();
 
 if (isInViewport(document.querySelector(".containerBackground"))) {
   console.log("In viewport");
