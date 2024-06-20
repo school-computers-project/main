10a863fac4c8dd0895bbb80d327d96a634f34e73
pablonara <pablonara@pablonara.com>
2024-06-19 21:00:44 -0700

    Auto-commit

--- static/indexJs.js
+++ static/indexJs.js
@@ -79,6 +79,20 @@ ScrollTrigger.create({
   markers: false //remove when fix
 });
 
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  var html = document.documentElement;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
+}
+
+if isInViewport(document.querySelector('.centered')) {
+  console.log('in viewport');
+}
 
 var tl,
     bgColor = "white",
@@ -150,3 +164,4 @@ tl.fromTo("#left-side", 1,
     background: bgColor
    }
 );
+
