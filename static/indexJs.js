ba363d671e627d309dd2caaf4251de06ad85a419
pablonara <pablonara@pablonara.com>
2024-06-19 21:05:34 -0700

    Auto-commit

--- static/indexJs.js
+++ static/indexJs.js
@@ -104,7 +104,20 @@ ScrollTrigger.create({
   }
 });
 
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  var html = document.documentElement;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
+}
 
+if (isInViewport(document.querySelector("#smooth-content"))) {
+  console.log("In viewport");
+}
 var tl,
     bgColor = "white",
     easing = Power0.easeNone;
